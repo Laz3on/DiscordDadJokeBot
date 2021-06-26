e2f28868931aa93c77005b1b7ed0d4d18b7c66e0
Chris Lasley <laz3on@gmail.com>
2021-06-26 23:54:46 +0000

    implemented aiohttp for async / await requests

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import discord
 import requests
 import json
+import aiohttp
+import asyncio
 # from keep_alive import keep_alive
 
 client = discord.Client()
@@ -15,6 +17,14 @@ def get_dadjoke():
     json_data = json.loads(resp.text)
     joke = json_data['joke']
     return (joke)
+
+async def get_async_dadjoke():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as resp:
+            json_data = json.loads(await resp.text())
+            joke1 = json_data['joke']
+            return (joke1)
+
 
 @client.event
 async def on_ready():
@@ -30,6 +40,10 @@ async def on_message(message):
     if msg.startswith('$joke'):
         joke = get_dadjoke()
         await message.channel.send(joke)
+
+    if msg.startswith('$asyncjoke'):
+        joke1 = get_dadjoke()
+        await message.channel.send(joke1)
 
 my_secret = os.environ['token']
 
