4cb1be72342cc35c78456e1014e6acf4de1f2473
Chris Lasley <laz3on@gmail.com>
2021-06-26 16:46:03 +0000

    Initial Commit of the DiscordDadJoke Bot

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import discord
+import requests
+import json
+# from keep_alive import keep_alive
+
+client = discord.Client()
+
+url = "https://icanhazdadjoke.com/"
+headers = {'Accept': 'application/json'}
+params = ''
+
+def get_dadjoke():
+    resp = requests.get(url, headers=headers)
+    json_data = json.loads(resp.text)
+    joke = json_data['joke']
+    return (joke)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    msg = message.content
+
+    if msg.startswith('$joke'):
+        joke = get_dadjoke()
+        await message.channel.send(joke)
+
+my_secret = os.environ['token']
+
+# keep_alive()
+client.run(my_secret)
