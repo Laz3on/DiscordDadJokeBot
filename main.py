d5c554244efbe2baef0ed75ba4a53db7a1971e58
Chris Lasley <laz3on@gmail.com>
2021-06-27 02:54:14 +0000

    reverted back to using requests

--- main.py
+++ main.py
@@ -2,29 +2,18 @@ import os
 import discord
 import requests
 import json
-import aiohttp
-import asyncio
 # from keep_alive import keep_alive
 
 client = discord.Client()
 
 url = "https://icanhazdadjoke.com/"
 headers = {'Accept': 'application/json'}
-params = ''
 
 def get_dadjoke():
     resp = requests.get(url, headers=headers)
     json_data = json.loads(resp.text)
     joke = json_data['joke']
     return (joke)
-
-async def get_async_dadjoke():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as resp:
-            json_data = json.loads(await resp.text())
-            joke1 = json_data['joke']
-            return (joke1)
-
 
 @client.event
 async def on_ready():
@@ -40,10 +29,6 @@ async def on_message(message):
     if msg.startswith('$joke'):
         joke = get_dadjoke()
         await message.channel.send(joke)
-
-    if msg.startswith('$asyncjoke'):
-        joke1 = get_dadjoke()
-        await message.channel.send(joke1)
 
 my_secret = os.environ['token']
 
